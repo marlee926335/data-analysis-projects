821fee6d6a5e75ebeab214b9997338d0859a9d51
Marcus Lee <marcuslee926335@gmail.com>
2025-11-04 19:40:51 -0600

    committing all updates/completions

--- data-and-variables/more-on-strings.py
+++ data-and-variables/more-on-strings.py
@@ -1,2 +1,9 @@
 print(type("17"))
 print(type("3.2"))
+
+print("Hello" + input('please enter name'))
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+
+print(num1 + num2)
